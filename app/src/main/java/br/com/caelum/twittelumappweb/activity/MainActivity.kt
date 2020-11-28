76626c327ba4b515398ef9be0be51a6de37e38ac
Matheus Brandino <m.v.brandino@gmail.com>
2020-11-28 13:11:53 -0300

    use viewbinding in main activity

--- app/src/main/java/br/com/caelum/twittelumappweb/activity/MainActivity.kt
+++ app/src/main/java/br/com/caelum/twittelumappweb/activity/MainActivity.kt
@@ -2,12 +2,13 @@ package br.com.caelum.twittelumappweb.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import br.com.caelum.twittelumappweb.R
+import br.com.caelum.twittelumappweb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
